86fb362e1383146d19a6b9ea12a9ad70a6488cc8
webbot-in <webbot.in@gmail.com>
2024-04-19 22:40:39 +0530

    initial commit

---
+++ src/pages/Loginpage/Login.js
@@ -0,0 +1,16 @@
+import { Card, Stack } from '@mui/material'
+import React from 'react'
+
+function Login() {
+    return (
+        <>
+            <Stack alignItems={'center'} justifyContent={'center'}>
+                <Card>Login
+
+                </Card>
+            </Stack>
+        </>
+    )
+}
+
+export default Login
